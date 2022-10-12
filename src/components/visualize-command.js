165eb164025cc2abfe27322374634deef8079af7
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-12 12:18:07 +0800

    Handle qcf special command

--- src/components/visualize-command.js
+++ src/components/visualize-command.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { purple, grey } from '@ant-design/colors'
+import { purple, grey } from "@ant-design/colors";
+
 
 const VisualizeCommand = ({ data }) => {
   const [commands, setCommands] = useState([]);
+
+  const SP_CMD = {
+    qcf: ["d", "df", "f"],
+    qcb: ["d", "df", "f"],
+    dp: ["f", "d", "df"],
+  };
 
   useEffect(() => {
     setCommands(sanitizeCommand(data));
   }, [data]);
 
-  function sanitizeCommand(cmdStr){
+  function sanitizeCommand(cmdStr) {
     // remove all spaces
     const dataNoSpaces = data.toLowerCase().replace(/\s/g, "");
     // artificially add the limit symbol for pseudo command so we can split properly without user writing this themselves
-    const addLimiterForAngleSymbol = dataNoSpaces.replace('>', ",>,");
+    const addLimiterForAngleSymbol = dataNoSpaces.replace(">", ",>,");
     // split into array by . and , because '>' is actually a pseudo command
     const splitCommands = addLimiterForAngleSymbol.split(/[\.,]+/);
     // remove all empty elements
@@ -21,36 +28,42 @@ const VisualizeCommand = ({ data }) => {
     return sanitizedCommands;
   }
 
-  const renderCommand = cmd => <img style={styles.command} src={require(`../assets/${cmd}.svg`)} />
+  const renderCommand = (cmd) => {
+    console.log('render',cmd)
+    return (
+      <img style={styles.command} src={require(`../assets/${cmd}.svg`)} />
+    )
+  };
 
   return (
     <div style={styles.container}>
       {
-        commands.map((command) => {
-          switch(command){
+        commands.map( command  => {
+          switch (command) {
+            case "qcf":
+              return SP_CMD.qcf.map(c => renderCommand(c))
+            break;
             default:
-              return renderCommand(command)
+              return renderCommand(command);
           }
         })
       }
     </div>
   );
-};
-
-
+}
 
 const styles = {
   container: {
     display: "flex",
-    padding : 16,
-    minHeight : 88,
+    padding: 16,
+    minHeight: 88,
     background: grey[2],
-    borderRadius : 8,
-    border: `2px solid ${purple}`
+    borderRadius: 8,
+    border: `2px solid ${purple}`,
   },
   command: {
-    width: 32
-  }
+    width: 32,
+  },
 };
 
 export default VisualizeCommand;
