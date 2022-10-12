33ac41b0ab70b5445f870b616fb7611099a3de44
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-12 12:18:07 +0800

    Allow uppercase direction notation to indicate (hold) for the button

--- src/components/visualize-command.js
+++ src/components/visualize-command.js
@@ -18,13 +18,27 @@ const VisualizeCommand = ({ data, options = {} }) => {
 
   function sanitizeCommand(cmdstring) {
     // remove all spaces
-    const dataNoSpaces = cmdstring.toLowerCase().replace(/\s/g, "");
+    const dataNoSpaces = cmdstring.replace(/\s/g, "");
     // artificially add the limit symbol for pseudo command so we can split properly without user writing this themselves
     const addLimiterForAngleSymbol = dataNoSpaces.replace(/>/g, ",>,");
     // split into array by . and + and , because '>' is actually a pseudo command we can't split by this symbol
     const splitCommands = addLimiterForAngleSymbol.split(/[\.,\+]+/);
     // remove all empty elements
     const sanitizedCommands = splitCommands.filter((c) => c);
+
+    const numCmds = sanitizedCommands.length;
+    for(let i = 0; i < numCmds; i++){ 
+      let cmd = sanitizedCommands[i];
+      try {
+        ['F','D','B','U'].forEach(notation => {
+          if (cmd.includes(notation)){
+            sanitizedCommands[i] = cmd.toLowerCase() + 'p';
+            throw new Error('Not an Error');
+          }
+        })
+      }
+      catch(err){} // do nothing, just used to break forEach.
+    }
 
     return sanitizedCommands;
   }
