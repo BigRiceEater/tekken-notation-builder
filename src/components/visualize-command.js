fc71e1a88e3e3ef10f6240f0ce8d6d3cd7fc5eeb
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-12 12:18:07 +0800

    Move sanitizing command string to another folder to separate the concern

--- src/components/visualize-command.js
+++ src/components/visualize-command.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { purple, grey } from "@ant-design/colors";
 
 import ComboSeperator from "./combo-seperator";
+import { sanitize } from '../util/commands'
 
 const VisualizeCommand = ({ data, options = {} }) => {
   const [commands, setCommands] = useState([]);
@@ -13,35 +14,8 @@ const VisualizeCommand = ({ data, options = {} }) => {
   };
 
   useEffect(() => {
-    setCommands(sanitizeCommand(data));
+    setCommands(sanitize(data));
   }, [data]);
-
-  function sanitizeCommand(cmdstring) {
-    // remove all spaces
-    const dataNoSpaces = cmdstring.replace(/\s/g, "");
-    // artificially add the limit symbol for pseudo command so we can split properly without user writing this themselves
-    const addLimiterForAngleSymbol = dataNoSpaces.replace(/>/g, ",>,");
-    // split into array by . and + and , because '>' is actually a pseudo command we can't split by this symbol
-    const splitCommands = addLimiterForAngleSymbol.split(/[\.,\+]+/);
-    // remove all empty elements
-    const sanitizedCommands = splitCommands.filter((c) => c);
-
-    const numCmds = sanitizedCommands.length;
-    for(let i = 0; i < numCmds; i++){ 
-      let cmd = sanitizedCommands[i];
-      try {
-        ['F','D','B','U'].forEach(notation => {
-          if (cmd.includes(notation)){
-            sanitizedCommands[i] = cmd.toLowerCase() + 'p';
-            throw new Error('Not an Error');
-          }
-        })
-      }
-      catch(err){} // do nothing, just used to break forEach.
-    }
-
-    return sanitizedCommands;
-  }
 
   const renderCommand = (cmd) => {
     console.log("render", cmd);
