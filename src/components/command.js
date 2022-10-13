21aa619df0fe636c9f3e2e030ff801cba04d2f66
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-13 18:51:12 +0800

    Add more svg to manage in Command component but a lot of importing code twice

--- src/components/command.js
+++ src/components/command.js
@@ -1,12 +1,40 @@
+import { icons } from "antd/lib/image/PreviewGroup";
 import one from "../assets/1.svg";
+import two from "../assets/2.svg";
+import three from "../assets/3.svg";
+import four from "../assets/4.svg";
 
-const Command = ({options = {}})=>{
+const Command = ({children, options = {}})=>{
+
+  var iconSource = require('../assets/1.svg');
+  switch(children){
+    case IconName.one : 
+      iconSource = one;
+    break;
+    case IconName.two :
+      iconSource = two;
+      break;
+    case IconName.three:
+      iconSource = three;
+      break;
+    case IconName.four:
+      iconSource = four;
+      break;
+  }
+
   return(
     <img
     style={{ width: options.biggerCommands ? 64 : 32 }}
-    src={one}
+    src={iconSource}
     />
   )
 }
 
 export default Command;
+
+export const IconName = {
+  one : "one",
+  two : "two",
+  three : "three",
+  four : "four",
+}
