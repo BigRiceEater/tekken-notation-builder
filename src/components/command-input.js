338003d574a29036edfe6a3a0dd171f010f4c469
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-13 18:30:17 +0800

    Give default size to input to stop mobile browsers zooming in

--- src/components/command-input.js
+++ src/components/command-input.js
@@ -9,11 +9,12 @@ const CommandInput = ({ onClick }) => {
     <Row gutter={[16,0]}>
       <Col span={18}>
         <Input
+          style={{fontSize : 16}}
           placeholder="Command Notation"
           onChange={(event) => setCommandNotation(event.target.value)}
         />
       </Col>
-      <Col span={6}>
+      <Col span={6} style={{alignSelf : "center"}}>
         <Button block type="primary" onClick={() => onClick(commandNotation)}>
           Go!
         </Button>
