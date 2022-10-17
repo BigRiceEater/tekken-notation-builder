66305871d815d48573d5a41c84dd9ca5a08e879b
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-17 12:03:30 +0800

    Rename svg files to match mapping in Command component

--- src/components/command.js
+++ src/components/command.js
@@ -1,45 +1,66 @@
-import one from "../assets/1.svg";
-import two from "../assets/2.svg";
-import three from "../assets/3.svg";
-import four from "../assets/4.svg";
-
 // https://github.com/facebook/create-react-app/issues/9831#issuecomment-716063951
 /* 
   Failing to use craco to indicate these image files are not es Modules, the next best 
   workaround is by using the .default when declaring the require() for now.
+  
+  const image = require('../assets/one.svg).default
+
+  but strangely when directly using in image src attriute, default is not needed.
 */
 
-const Command = ({children, options = {}})=>{
-
-  var iconSource = require('../assets/1.svg').default;
-  switch(children){
-    case IconName.one : 
-      iconSource = one;
-    break;
-    case IconName.two :
-      iconSource = two;
-      break;
-    case IconName.three:
-      iconSource = three;
-      break;
-    case IconName.four:
-      iconSource = four;
-      break;
-  }
-
-  return(
+// 'icon' should be given the CommandName to find the corresponding filename to load
+const Command = ({ icon, options = {} }) => {
+  return (
     <img
-    style={{ width: options.biggerCommands ? 64 : 32 }}
-    src={iconSource}
+      style={{ width: options.biggerCommands ? 64 : 32 }}
+      src={require(`../assets/${icon}.svg`)}
     />
-  )
-}
+  );
+};
 
 export default Command;
 
-export const IconName = {
-  one : "one",
-  two : "two",
-  three : "three",
-  four : "four",
-}
+// map the command names to their filename counterpart so we don't have to write a very long switch case.
+export const CommandName = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+
+  onetwo: "1+2",
+  onethree: "1+3",
+  onefour: "1+4",
+
+  twothree: "2+3",
+  twofour: "2+4",
+
+  threefour: "3+4",
+
+  forward: "f",
+  fowardhold: "fp",
+  back: "b",
+  backhold: "bp",
+  up: "u",
+  uphold: "up",
+  down: "d",
+  downhold: "dp",
+
+  downforward: "df",
+  downfowardhold: "dfp",
+  downback: "db",
+  downbackhold: "dbp",
+  upforward: "uf",
+  upforwardhold: "ufp",
+  upback: "ub",
+  upbackhold: "ubp",
+
+  dash: "dash",
+  whilestanding: "ws",
+  sidestepleft: "ssl",
+  sidestepright: "ssr",
+  launch: "launch",
+  run: "run",
+  screw: "screw",
+  delay: "delay",
+  wall: "wall",
+};
