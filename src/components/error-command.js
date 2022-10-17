7e8d506262fa612b739c08ce8230581c743559a4
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-17 13:29:37 +0800

    Configure toast

--- src/components/error-command.js
+++ src/components/error-command.js
@@ -3,14 +3,22 @@ import { WarningTwoTone } from "@ant-design/icons";
 import { toast } from "react-toastify";
 
 const ErrorCommand = ({ cmd, options }) => {
-  
   // The alert will only show once in the whole lifetime of this particular instances existence
   const showAlert = useRef(true);
 
   useEffect(() => {
-    if (showAlert.current){
-      toast(`${cmd} is wrong!`);
-      showAlert.current = false
+    if (showAlert.current) {
+      toast.error(`${cmd} is not recognised`, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      showAlert.current = false;
     }
   }, []);
 
--- src/routes/root.js
+++ src/routes/root.js
@@ -1,39 +1,49 @@
-import { Outlet } from "react-router-dom"
-import { Layout , Row, Divider} from 'antd';
+import { Outlet } from "react-router-dom";
+import { Layout, Row, Divider } from "antd";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import packageData from "../../package.json"
+import "react-toastify/dist/ReactToastify.css";
+import packageData from "../../package.json";
 
 const { Content, Footer } = Layout;
 
-const Root = () =>{
+const Root = () => {
   return (
-   <Layout style={styles.container}>
-     <Content>
-       <Row justify="center" gutter={[16, 16]}>
-         <Outlet />
-       </Row>
-     </Content>
-     <Footer>
-       <Divider orientation="center" plain>Version {packageData.version}</Divider>
-     </Footer>
-     <ToastContainer />
-   </Layout>
- );
-}
+    <Layout style={styles.container}>
+      <Content>
+        <Row justify="center" gutter={[16, 16]}>
+          <Outlet />
+        </Row>
+      </Content>
+      <Footer>
+        <Divider orientation="center" plain>
+          Version {packageData.version}
+        </Divider>
+      </Footer>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </Layout>
+  );
+};
 
 const styles = {
-  container : {
-    height:"100%",
+  container: {
+    height: "100%",
     paddingTop: 24,
-    paddingBottom : 0,
+    paddingBottom: 0,
     paddingLeft: 24,
-    paddingRight: 24
+    paddingRight: 24,
   },
-  footer : {
-    
-  }
-}
-
+  footer: {},
+};
 
 export default Root;
