330d96523cea02315faa30feb79e66b1c94da942
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-18 11:26:13 +0800

    Convert visualized commands to png and show on page

--- src/components/visualize-command.js
+++ src/components/visualize-command.js
@@ -6,7 +6,12 @@ import ComboSeperator from "./combo-seperator";
 
 import { sanitize } from "../util/commands";
 
+import * as htmlToImage from "html-to-image";
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
+
 const VisualizeCommand = ({ commands = [], options = {} }) => {
+
+  const [commandsImage, setCommandsImage] = useState(null);
 
   const SP_CMD = {
     qcf: ["d", "df", "f"],
@@ -18,34 +23,52 @@ const VisualizeCommand = ({ commands = [], options = {} }) => {
     return <Command icon={cmd} options={options} />;
   };
 
+  useEffect(() => {
+    const imageContainer = document.getElementById("image-container");
+    const commandsContainer = document.getElementById("commands-container");
+    const oldImage = document.getElementById('commands-as-image');
+    htmlToImage.toPng(commandsContainer).then((dataUrl) => {
+      // const img = new Image();
+      // img.src = dataUrl;
+      // imageContainer.replaceChild(img,oldImage);
+      setCommandsImage(dataUrl);
+    });
+  });
+
   return (
-    <div
-      style={{
-        ...styles.container,
-        background: options.whiteBackgroundChecked ? "white" : grey[2],
-      }}>
-      {commands.map((command, idx) => {
-        switch (command) {
-          case "qcf":
-            return;
-            <React.Fragment key={idx}>
-              {SP_CMD.qcf.map((cmd) => renderCommand(cmd))}
-            </React.Fragment>;
-            break;
-          case ">":
-            return (
-              <ComboSeperator
-                key={idx}
-                invertColor={options.whiteBackgroundChecked}
-                bigger={options.biggerCommands}
-              />
-            );
-            break;
-          default:
-            return renderCommand(command);
-        }
-      })}
-    </div>
+    <React.Fragment>
+      <div
+        id="commands-container"
+        style={{
+          ...styles.container,
+          background: options.whiteBackgroundChecked ? "white" : grey[2],
+        }}>
+        {commands.map((command, idx) => {
+          switch (command) {
+            case "qcf":
+              return;
+              <React.Fragment key={idx}>
+                {SP_CMD.qcf.map((cmd) => renderCommand(cmd))}
+              </React.Fragment>;
+              break;
+            case ">":
+              return (
+                <ComboSeperator
+                  key={idx}
+                  invertColor={options.whiteBackgroundChecked}
+                  bigger={options.biggerCommands}
+                />
+              );
+              break;
+            default:
+              return renderCommand(command);
+          }
+        })}
+      </div>
+      <div id="image-container" style={styles.imageContainer}>
+        <img id="commands-as-image" src={commandsImage} />
+      </div>
+    </React.Fragment>
   );
 };
 
@@ -63,6 +86,9 @@ const styles = {
   command: {
     width: 32,
   },
+  imageContainer:{
+    
+  }
 };
 
 export default VisualizeCommand;
