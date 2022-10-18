5e7ca1effb06715cf7131f3d5be160325298c5cb
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-18 16:58:11 +0800

    Hide the copy to clipboard for mobiles because it just doesn't work

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Space } from "antd";
 import { CopyOutlined } from "@ant-design/icons";
 import Checkbox from "./control-checkbox";
 
+
 const Controls = ({ data, onChange, onClipboardClick }) => {
+
+  const [showClipboardButton , setShowClipboardButton] = useState(false)
+
+  useEffect(()=>{
+    const isMobile = window.innerWidth <= 576 ;
+    setShowClipboardButton(!isMobile);
+  })
+
   return (
     <Space>
-      <Button
-        type="default"
-        icon={<CopyOutlined />}
-        onClick={onClipboardClick}></Button>
-
+      {showClipboardButton && (
+        <Button
+          type="default"
+          icon={<CopyOutlined />}
+          onClick={onClipboardClick}></Button>
+      )}
       <Checkbox
         controlName="whiteBackgroundChecked"
         checked={data.whiteBackgroundChecked}
