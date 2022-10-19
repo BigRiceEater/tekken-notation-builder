b2aafacfc870c40b8439d6ee5e4e04799e6a7e18
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-19 11:30:31 +0800

    Remove unused App file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
+import 'antd/dist/antd.css';
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {
   createHashRouter as createRouter,
