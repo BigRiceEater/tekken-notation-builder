652070ed9fc1f7b48041e3464f66a7f2d23b7518
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-20 18:28:10 +0800

    Fix color of header to match page

--- src/layout/header.js
+++ src/layout/header.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useRecoilState } from 'recoil';
 import { Layout, Space, Button } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
+import { grey } from "@ant-design/colors"
 
 import { AppDrawer } from "./drawer";
 import { appMenuStore } from "../store/frontend";
@@ -12,7 +13,7 @@ export const AppHeader = () => {
   const [openMenu, setOpenMenu] = useRecoilState(appMenuStore);
   return (
     <>
-      <Header style={{ backgroundColor: "white", padding: "0px 24px" }}>
+      <Header style={{ backgroundColor: "#F0F2F5", padding: "0px 24px" }}>
         <Space>
           <Button
             type="text"
