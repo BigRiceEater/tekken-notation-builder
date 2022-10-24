bca898d8d5129ea1cc2067d71103da8bd9cf0c39
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-24 16:34:13 +0800

    Add Icons for menu

--- src/layout/menu.js
+++ src/layout/menu.js
@@ -1,9 +1,21 @@
 import { useRecoilState } from "recoil";
 import { Link, useLocation } from "react-router-dom";
-import { Menu } from "antd";
+import { Menu, Space } from "antd";
 
 import { appMenuStore } from "../store/frontend";
 import { useEffect } from "react";
+import {
+  AppleOutlined,
+  HomeOutlined,
+  UnorderedListOutlined,
+} from "@ant-design/icons";
+
+const AppLink = ({ icon, label }) => (
+  <Space>
+    {icon}
+    {label}
+  </Space>
+);
 
 export const AppMenu = () => {
   const [openMenu, setOpenMenu] = useRecoilState(appMenuStore);
@@ -11,8 +23,22 @@ export const AppMenu = () => {
 
   // key must match the pathname of location from react-router-dom
   const items = [
-    { label: <Link to="">Home</Link>, key: "/" },
-    { label: <Link to="changelog">Changelog</Link>, key: "/changelog" },
+    {
+      label: (
+        <Link to="">
+          <AppLink icon={<HomeOutlined />} label="Home" />
+        </Link>
+      ),
+      key: "/",
+    },
+    {
+      label: (
+        <Link to="changelog">
+          <AppLink icon={<UnorderedListOutlined />} label="Changelog" />
+        </Link>
+      ),
+      key: "/changelog",
+    },
   ];
 
   return (
