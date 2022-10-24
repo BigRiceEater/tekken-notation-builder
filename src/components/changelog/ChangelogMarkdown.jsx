19f54a1dd3bfd49bb74e45e9fe338558c568b0fc
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-24 16:37:39 +0800

    Update changelog

--- src/components/changelog/ChangelogMarkdown.jsx
+++ src/components/changelog/ChangelogMarkdown.jsx
@@ -10,7 +10,12 @@ export const ChangelogMarkdown = () => {
             margin: '0 auto',
             padding: 15
           }}>
-            <h1>Version 0.6.0</h1>
+            <h1>Version 0.7.0</h1>
+<ul>
+<li>Implement new Menu using another library with animation and better mobile experience.</li>
+<li>Add Icon for each menu link.</li>
+</ul>
+<h1>Version 0.6.0</h1>
 <ul>
 <li>Add Menu to navigate to other pages.</li>
 <li>Add changelog to keep track of version releases.</li>
