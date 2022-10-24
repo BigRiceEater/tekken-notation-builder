d564703fb5a3b829a61b506b529eafed2ab5b8a5
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-24 16:06:47 +0800

    Fix menu not highlighted correctly when changing location

--- src/layout/menu.js
+++ src/layout/menu.js
@@ -1,16 +1,26 @@
 import { useRecoilState } from "recoil";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Menu } from "antd";
 
 import { appMenuStore } from "../store/frontend";
+import { useEffect } from "react";
 
 export const AppMenu = () => {
-  const [openMenu, setOpenMenu] = useRecoilState(appMenuStore)
+  const [openMenu, setOpenMenu] = useRecoilState(appMenuStore);
+  const location = useLocation();
 
+  // key must match the pathname of location from react-router-dom
   const items = [
-    { label: <Link to="">Home</Link>, key: "home" },
-    { label: <Link to="changelog">Changelog</Link>, key: "changelog" },
+    { label: <Link to="">Home</Link>, key: "/" },
+    { label: <Link to="changelog">Changelog</Link>, key: "/changelog" },
   ];
 
-  return <Menu style={{height: "100%", border : "0px"}} items={items} onClick={()=> setOpenMenu(false)}/>;
+  return (
+    <Menu
+      style={{ height: "100%", border: "0px" }}
+      items={items}
+      onClick={() => setOpenMenu(false)}
+      selectedKeys={location.pathname}
+    />
+  );
 };
