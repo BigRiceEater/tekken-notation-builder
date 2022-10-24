445af7ce9ed4f5f9ef402c07f8277abef6697794
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-24 11:02:20 +0800

    Update changelog

--- src/components/changelog/ChangelogMarkdown.jsx
+++ src/components/changelog/ChangelogMarkdown.jsx
@@ -10,50 +10,10 @@ export const ChangelogMarkdown = () => {
             margin: '0 auto',
             padding: 15
           }}>
-            <h2>
-<a className="anchor" href="#0.3.0" id="0.3.0">
-<svg aria-hidden="true" className="octicon octicon-link" height="16" version="1.1" viewbox="0 0 16 16" width="16">
-  <path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
-</svg>
-</a>[0.3.0] - 2003-03-03</h2>
-<h3>
-<a className="anchor" href="#0.3.0-added" id="0.3.0-added">
-<svg aria-hidden="true" className="octicon octicon-link" height="16" version="1.1" viewbox="0 0 16 16" width="16">
-  <path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
-</svg>
-</a>Added</h3>
+            <h1>Version 0.6.0</h1>
 <ul>
-<li>A bullet.</li>
-</ul>
-<h2>
-<a className="anchor" href="#0.2.0" id="0.2.0">
-<svg aria-hidden="true" className="octicon octicon-link" height="16" version="1.1" viewbox="0 0 16 16" width="16">
-  <path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
-</svg>
-</a>[0.2.0] - 2002-02-02</h2>
-<h3>
-<a className="anchor" href="#0.2.0-added" id="0.2.0-added">
-<svg aria-hidden="true" className="octicon octicon-link" height="16" version="1.1" viewbox="0 0 16 16" width="16">
-  <path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
-</svg>
-</a>Added</h3>
-<ul>
-<li>Another bullet.</li>
-</ul>
-<h2>
-<a className="anchor" href="#0.1.0" id="0.1.0">
-<svg aria-hidden="true" className="octicon octicon-link" height="16" version="1.1" viewbox="0 0 16 16" width="16">
-  <path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
-</svg>
-</a>[0.1.0] - 2001-01-01</h2>
-<h3>
-<a className="anchor" href="#0.1.0-added" id="0.1.0-added">
-<svg aria-hidden="true" className="octicon octicon-link" height="16" version="1.1" viewbox="0 0 16 16" width="16">
-  <path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
-</svg>
-</a>Added</h3>
-<ul>
-<li>Last bullet.</li>
+<li>Add Menu to navigate to other pages.</li>
+<li>Add changelog to keep track of version releases.</li>
 </ul>
 
         </div>
