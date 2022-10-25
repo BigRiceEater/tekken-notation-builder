3d19943a8bc3dbce73ae740ed5789ae7839661ae
Big.Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-10-25 16:21:51 +0800

    Remove command's background higlight when finish dragging

--- src/components/drag-commands.js
+++ src/components/drag-commands.js
@@ -45,11 +45,9 @@ export const DragCommands = () => {
   const getItemStyle = (isDragging, draggableStyle) => ({
     // some basic styles to make the items look a bit nicer
     userSelect: "none",
-    padding: grid * 2,
-    margin: `0 ${grid}px 0 0`,
-
+    
     // change background colour if dragging
-    background: isDragging ? "lightgreen" : "grey",
+    background: isDragging ? "lightgreen" : "#00000000",
 
     // styles we need to apply on draggables
     ...draggableStyle,
